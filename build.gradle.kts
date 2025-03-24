000c9748393b9935b2370706841b2fd75103c46b
mobin.pourabedini <realmobinpourabedini@gmail.com>
2025-03-24 22:21:41 +0330

    add retrofit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,21 @@ plugins {
 }
 
 group = "org.example"
-version = "1.0.0"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))  // Kotlin standard library
+    implementation("com.google.code.gson:gson:2.8.8")  // Gson
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")  // OkHttp Logging Interceptor
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")  // Gson converter
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")  // Retrofit
     testImplementation(kotlin("test"))
     implementation("com.github.ajalt.clikt:clikt:4.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,70 @@
-package org.example
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import java.util.*
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun getGitHubToken(): String {
+    return System.getenv("GITHUB_TOKEN") ?: throw IllegalStateException("GITHUB_TOKEN is not set!")
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+// Data Models
+data class GitHubUser(
+    val login: String,
+    val followers: Int,
+    val following: Int,
+    @SerializedName("created_at") val createdAt: String
+)
+
+data class Repository(
+    val name: String
+)
+
+// API Service
+interface GitHubApi {
+    @GET("users/{username}")
+    suspend fun getUser(@Path("username") username: String): GitHubUser
+
+    @GET("users/{username}/repos")
+    suspend fun getUserRepos(@Path("username") username: String): List<Repository>
+}
+
+// Retrofit Client
+fun createGitHubService(authToken: String): GitHubApi {
+    val logging = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+    val authInterceptor = Interceptor { chain ->
+        val originalRequest: Request = chain.request()
+        val authenticatedRequest = originalRequest.newBuilder()
+            .header("Authorization", "Bearer $authToken")  // Add Bearer Token here
+            .build()
+        chain.proceed(authenticatedRequest)
     }
+    val client = OkHttpClient.Builder().addInterceptor(logging).addInterceptor(authInterceptor).build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create(Gson()))
+        .build()
+
+    return retrofit.create(GitHubApi::class.java)
+}
+
+// CLI
+suspend fun main() {
+
+    val scanner = Scanner(System.`in`)
+    val api = createGitHubService(getGitHubToken())
+    val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create(Gson()))
+        .build()
+    var ret = retrofit.create(GitHubApi::class.java)
+    val user = ret.getUser("b4rd14")
+    println(user)
 }
